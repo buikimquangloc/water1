a2153fa36364c9b9904e9c9fde86dd4d576afa4a
buikimquangloc <118284013+buikimquangloc@users.noreply.github.com>
2024-08-19 16:40:54 +0700

    Connect DB

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ async function startServer() {
 // connection.query(
 //     "select * from sanpham",
 //     function(err, results, fields) {
-//         if (err) throw err;
+//         // if (err) throw err;
 //         console.log('-> kết quả: ', results);
 //     }
 // )
